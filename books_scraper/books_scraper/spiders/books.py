a915c5276c67da0a7e5dd51ddf649584fabb02b3
Oleksandr Moroz <molecxander@gmail.com>
2024-07-06 20:41:40 +0300

    Solution

--- books_scraper/books_scraper/spiders/books.py
+++ books_scraper/books_scraper/spiders/books.py
@@ -15,34 +15,34 @@ class BooksSpider(scrapy.Spider):
         if next_page is not None:
             yield response.follow(next_page, self.parse)
 
-    def parse_book(self, response):
+    def parse_book(self, response: Response):
         yield {
-            'title': self.parse_title(response),
-            'price': self.parse_price(response),
-            'amount_in_stock': self.parse_amount_in_stock(response),
-            'rating': self.parse_rating(response),
-            'category': self.parse_category(response),
-            'description': self.parse_description(response),
-            'upc': self.parse_upc(response),
+            "title": self.parse_title(response),
+            "price": self.parse_price(response),
+            "amount_in_stock": self.parse_amount_in_stock(response),
+            "rating": self.parse_rating(response),
+            "category": self.parse_category(response),
+            "description": self.parse_description(response),
+            "upc": self.parse_upc(response),
         }
 
-    def parse_title(self, response):
-        return response.css('h1::text').get()
+    def parse_title(self, response: Response):
+        return response.css("h1::text").get()
 
-    def parse_price(self, response):
-        return response.css('p.price_color::text').get()
+    def parse_price(self, response: Response):
+        return response.css("p.price_color::text").get()
 
-    def parse_amount_in_stock(self, response):
-        return response.css('p.instock.availability::text').re_first('\d+')
+    def parse_amount_in_stock(self, response: Response):
+        return response.css("p.instock.availability::text").re_first("\d+")
 
-    def parse_rating(self, response):
-        return response.css('p.star-rating::attr(class)').re_first('star-rating (\w+)')
+    def parse_rating(self, response: Response):
+        return response.css("p.star-rating::attr(class)").re_first("star-rating (\w+)")
 
-    def parse_category(self, response):
-        return response.css('ul.breadcrumb li a::text')[-2].get()
+    def parse_category(self, response: Response):
+        return response.css("ul.breadcrumb li a::text")[-2].get()
 
-    def parse_description(self, response):
+    def parse_description(self, response: Response):
         return response.css('meta[name="description"]::attr(content)').get().strip()
 
-    def parse_upc(self, response):
-        return response.css('table.table-striped tr:nth-child(1) td::text').get()
+    def parse_upc(self, response: Response):
+        return response.css("table.table-striped tr:nth-child(1) td::text").get()
